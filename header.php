7ba384ce8ad0a79ea6c3d7a016045070fcc33937
dijojperumaly <dijoperumalil@gmail.com>
2026-02-13 02:35:49 +0530

    services updated

--- header.php
+++ header.php
@@ -89,10 +89,10 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <div class="navbar-nav ms-auto py-0">
-                    <a href="index.php" class="nav-item nav-link">Home</a>
+                    <a href="index.php" class="nav-item nav-link active">Home</a>
                     <a href="about.php" class="nav-item nav-link">About Us</a>
                     <a href="services.php" class="nav-item nav-link">Our Services</a>
-                    <a href="contact.php" class="nav-item nav-link active">Contact Us</a>
+                    <a href="contact.php" class="nav-item nav-link">Contact Us</a>
                 <!--<div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Blog</a>
                         <div class="dropdown-menu m-0">
--- services.php
+++ services.php
@@ -118,7 +118,7 @@ include("header.php");
 
 
     <!-- Service Start -->
-    <!-- <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
                 <h5 class="fw-bold text-primary text-uppercase">Our Services</h5>
@@ -194,7 +194,7 @@ include("header.php");
                 </div>
             </div>
         </div>
-    </div> -->
+    </div>
     <!-- Service End -->
 
 
